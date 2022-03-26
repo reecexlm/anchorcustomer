542659ff9a587304a2800b40761386b7636d9d9c
Reece Markowsky <reecemarkowsky@Reeces-MacBook-Pro.local>
2022-03-26 14:41:40 -0700

    2 more workers

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -40,6 +40,20 @@ module "eks" {
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
       asg_desired_capacity          = 1
     },
+    {
+      name                          = "worker-group-5"
+      instance_type                 = "t2.micro"
+      additional_userdata           = "echo foo bar"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      asg_desired_capacity          = 1
+    },
+    {
+      name                          = "worker-group-6"
+      instance_type                 = "t2.micro"
+      additional_userdata           = "echo foo bar"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      asg_desired_capacity          = 1
+    },
   ]
 
   map_users = var.map_users
