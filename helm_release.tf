14db455a0a932dbac70cd44e09c04ff6f4f14041
Reece Markowsky <reecemarkowsky@Reeces-MacBook-Pro.local>
2022-03-26 12:31:44 -0700

    prom

--- helm_release.tf
+++ helm_release.tf
@@ -10,8 +10,14 @@ provider "helm" {
   }
 }
 
-resource "helm_release" "nginx-ingress" {
-  name       = "ingress-nginx"
-  repository = "https://kubernetes.github.io"
-  chart      = "ingress-nginx"
+resource "helm_release" "prometheus" {
+  name             = "prom"
+  chart            = "kube-prometheus-stack"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  namespace        = "monitoring"
+  version          = "17.1.3"
+  create_namespace = true
+  wait             = true
+  reset_values     = true
+  max_history      = 3
 }
