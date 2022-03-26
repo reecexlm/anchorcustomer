a37102ce47b7ea20b45cb8cb9f12ad3ec1424c3d
Reece Markowsky <reecemarkowsky@Reeces-MacBook-Pro.local>
2022-03-26 16:37:33 -0700

    anchor platform charts

---
+++ anchor-platform-reference-server-release.tf
@@ -0,0 +1,24 @@
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+      command     = "aws"
+    }
+  }
+}
+
+resource "helm_release" "prometheus" {
+  name             = "reference-server"
+  chart            = "kube-prometheus-stack"
+  repository       = "./anchor-platform-charts"
+  namespace        = "monitoring"
+  version          = "17.1.3"
+  create_namespace = true
+  wait             = true
+  reset_values     = true
+  max_history      = 3
+  timeout          = 600
+}
