cced44141b2a2ff5dee3d741c09e73431339a774
Reece Markowsky <reecemarkowsky@Reeces-MacBook-Pro.local>
2022-03-26 19:10:33 -0700

    anchor platform charts4

--- security-groups.tf
+++ security-groups.tf
@@ -4,8 +4,8 @@ resource "aws_security_group" "worker_group_mgmt_one" {
   vpc_id      = module.vpc.vpc_id
 
   ingress {
-    from_port = 22
-    to_port   = 22
+    from_port = 8081
+    to_port   = 8081
     protocol  = "tcp"
 
     cidr_blocks = [
@@ -19,8 +19,8 @@ resource "aws_security_group" "worker_group_mgmt_two" {
   vpc_id      = module.vpc.vpc_id
 
   ingress {
-    from_port = 22
-    to_port   = 22
+    from_port = 8081
+    to_port   = 8081
     protocol  = "tcp"
 
     cidr_blocks = [
