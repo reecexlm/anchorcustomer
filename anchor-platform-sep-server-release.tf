7a7c940eb59c435b362dd44a42216b96562bdfc9
Reece Markowsky <reecemarkowsky@Reeces-MacBook-Pro.local>
2022-04-01 12:52:37 -0700

    helm chart update

--- anchor-platform-sep-server-release.tf
+++ anchor-platform-sep-server-release.tf
@@ -1,15 +1,3 @@
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1alpha1"
-      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
-      command     = "aws"
-    }
-  }
-}
-
 resource "helm_release" "sep" {
   name             = "sep-server"
   chart            = "./charts/sep"
