a9db6f41b577c568ef1a2b2ad9f4b2938f5fe6ca
Reece Markowsky <reecemarkowsky@Reeces-MacBook-Pro.local>
2022-04-04 15:34:03 -0700

    eks

--- helm_release_nginx.tf
+++ helm_release_nginx.tf
@@ -1,7 +1,7 @@
 resource "helm_release" "nginx" {
   name        = "nginx"
   chart       = "nginx"
-  repository  = "./charts"
+  repository  = "./charts/nginx"
   namespace   = "nginx"
   max_history = 3
   create_namespace = true
