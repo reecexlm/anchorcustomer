a61bed239ef33a3983db449fae3c9c7bc0efcd81
Reece Markowsky <reecemarkowsky@Reeces-MacBook-Pro.local>
2022-04-04 16:45:37 -0700

    eks

--- alb.tf
+++ alb.tf
@@ -11,12 +11,6 @@ resource "aws_lb" "dev" {
   allocation_id = aws_eip.lb_eip.id
   enable_deletion_protection = true
 
-  access_logs {
-    bucket  = aws_s3_bucket.lb_logs.bucket
-    prefix  = "test-lb"
-    enabled = true
-  }
-
   tags = {
     Environment = "production"
   }
