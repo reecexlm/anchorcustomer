1dd3e6eea7f1a7e5ab1ffc66454f203a4b048a49
Reece Markowsky <reecemarkowsky@Reeces-MacBook-Pro.local>
2022-04-04 11:25:57 -0700

    eks

--- outputs.tf
+++ outputs.tf
@@ -20,7 +20,7 @@ output "kubectl_config" {
 
 output "config_map_aws_auth" {
   description = "A kubernetes configuration to authenticate to this EKS cluster."
-  value       = module.eks.config_map_aws_auth
+  value       = module.eks.aws_auth_configmap_yaml
 }
 
 output "region" {
