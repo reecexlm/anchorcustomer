2f420d0010861d5c55c51645e8b7fd875a47d79f
Reece Markowsky <reecemarkowsky@Reeces-MacBook-Pro.local>
2022-04-04 11:17:04 -0700

    eks

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,59 +1,48 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "17.24.0"
-  cluster_name    = local.cluster_name
-  cluster_version = "1.20"
-  subnets         = module.vpc.private_subnets
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 18.0"
 
-  vpc_id = module.vpc.vpc_id
+  cluster_name    = "anchor-platform"
+  cluster_version = "1.21"
 
-  workers_group_defaults = {
-    root_volume_type = "gp2"
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  cluster_addons = {
+    coredns = {
+      resolve_conflicts = "OVERWRITE"
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      resolve_conflicts = "OVERWRITE"
+    }
   }
 
-  worker_groups = [
-    {
-      name                          = "worker-group-1"
-      instance_type                 = "t2.micro"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-      asg_desired_capacity          = 1
-    },
-    {
-      name                          = "worker-group-2"
-      instance_type                 = "t2.micro"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 2
-    },
-    {
-      name                          = "worker-group-3"
-      instance_type                 = "t2.micro"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 2
-      tags = [{
-          key                 = "app"
-          value               = "sep"
-          propagate_at_launch = true
-      }]
-    },
-    {
-      name                          = "worker-group-4"
-      instance_type                 = "t2.micro"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 2
-      tags = [{
-          key                 = "app"
-          value               = "reference"
-          propagate_at_launch = true
-      }]
-    },
-  ]
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    instance_types = ["t2.micro"]
+  }
 
-  map_users = var.map_users
-}
+  eks_managed_node_groups = {
+    blue = {}
+    anchor_platform = {
+      min_size     = 1
+      max_size     = 1
+      desired_size = 1
+
+      instance_types = ["t2.micro"]
+      capacity_type  = "SPOT"
+    }
+    
+    reference = {
+      min_size     = 1
+      max_size     = 1
+      desired_size = 1
+
+      instance_types = ["t2.micro"]
+      capacity_type  = "SPOT"
+    }
+  }
 
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
