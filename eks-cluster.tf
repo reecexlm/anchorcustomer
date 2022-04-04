5a1662f512ef9fccf274b52f1d8771aa4aecbe54
Reece Markowsky <reecemarkowsky@Reeces-MacBook-Pro.local>
2022-04-04 12:12:41 -0700

    eks

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -22,6 +22,7 @@ module "eks" {
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     instance_types = ["t2.micro"]
+    subnet_ids = module.vpc.private_subnets
   }
 
   eks_managed_node_groups = {
