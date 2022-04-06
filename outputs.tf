ef84b46b6510d3461a2450b81ee92491fc4ca5f1
Reece Markowsky <reecemarkowsky@Reeces-MacBook-Pro.local>
2022-04-06 14:02:41 -0700

    public subnet

--- outputs.tf
+++ outputs.tf
@@ -8,14 +8,14 @@
 #  value       = module.eks.cluster_endpoint
 #}
 
-output "cluster_security_group_id" {
-  description = "Security group ids attached to the cluster control plane."
-  value       = module.eks.cluster_security_group_id
-}
-output "config_map_aws_auth" {
-  description = "A kubernetes configuration to authenticate to this EKS cluster."
-  value       = module.eks.aws_auth_configmap_yaml
-}
+#output "cluster_security_group_id" {
+#  description = "Security group ids attached to the cluster control plane."
+#  value       = module.eks.cluster_security_group_id
+#}
+#output "config_map_aws_auth" {
+#  description = "A kubernetes configuration to authenticate to this EKS cluster."
+#  value       = module.eks.aws_auth_configmap_yaml
+#}
 
 output "region" {
   description = "AWS region"
