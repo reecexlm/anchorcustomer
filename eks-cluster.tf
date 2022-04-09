f4956b2e842bd5a17060d1cae88f7b3959ca7ba2
Reece Markowsky <reecemarkowsky@Reeces-MacBook-Pro.local>
2022-04-08 17:41:29 -0700

    k8 version

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "~> 18.0"
 
   cluster_name    = "${local.cluster_name}"
-  cluster_version = "1.22.6"
+  cluster_version = "1.22"
 
   cluster_endpoint_private_access = true
   cluster_endpoint_public_access  = true
