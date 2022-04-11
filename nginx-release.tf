cac30b74808031e3022bd48b89e5213a7bb731d0
Reece Markowsky <reecemarkowsky@Reeces-MacBook-Pro.local>
2022-04-10 21:58:09 -0700

    add nginx

---
+++ nginx-release.tf
@@ -0,0 +1,16 @@
+resource "helm_release" "sep" {
+  name             = "sep-server"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  namespace        = "ingress-nginx"
+  version          = "17.1.3"
+  create_namespace = true
+  wait             = true
+  reset_values     = true
+  max_history      = 3
+  timeout          = 600
+
+    values = [
+    file("${path.module}/nginx-values.yaml")
+  ]
+}
