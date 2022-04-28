037774096833bfecb48960d014d8a80fe71999af
reecexlm <reecexlm@gmail.com>
2022-04-27 17:50:21 -0700

    min 4

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -33,9 +33,9 @@ module "eks" {
       
       capacity_type = "SPOT"
       instance_types = ["t3.nano", "t3.micro", "t3.small"]
-      desired_size = 2
-      min_size     = 1
-      max_size     = 4
+      desired_size = 4
+      min_size     = 4
+      max_size     = 5
 
         tags = {
           Environment = "dev"
