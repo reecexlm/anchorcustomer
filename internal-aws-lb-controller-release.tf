3208409dbb010513fdaf6461477268058fa01bd5
reecexlm <reecexlm@gmail.com>
2022-04-30 09:32:18 -0700

    add internal loadbalancer

---
+++ internal-aws-lb-controller-release.tf
@@ -0,0 +1,17 @@
+resource "helm_release" "ingress-nginx" {
+  name             = "aws-lb-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace        = "awsloadbalancer"
+  create_namespace = true
+  wait             = true
+  reset_values     = true
+  max_history      = 3
+  timeout          = 600
+  set {
+    name  = "controller.admissionWebhooks.enabled"
+    value = "false"
+  }
+}
+
+helm install aws-load-balancer-controller eks/aws-load-balancer-controller -n kube-system --set clusterName=<cluster-name> --set serviceAccount.create=false --set serviceAccount.name=aws-load-balancer-controller
