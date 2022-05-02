fb999ff7f8bd3a159b6ac8f76e15d1ede6137793
reecexlm <reecexlm@gmail.com>
2022-05-02 14:18:00 -0700

    eks role

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,3 +1,41 @@
+resource "aws_iam_role" "eks_cluster" {
+  # The name of the role
+  name = "eks-cluster"
+
+  # The policy that grants an entity permission to assume the role.
+  # Used to access AWS resources that you might not normally have access to.
+  # The role that Amazon EKS will use to create AWS resources for Kubernetes clusters
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+# Resource: aws_iam_role_policy_attachment
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
+
+resource "aws_iam_role_policy_attachment" "amazon_eks_cluster_policy" {
+  # The ARN of the policy you want to apply
+  # https://github.com/SummitRoute/aws_managed_policies/blob/master/policies/AmazonEKSClusterPolicy
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+
+  # The role the policy should be applied to
+  role = aws_iam_role.eks_cluster.name
+}
+
+# Resource: aws_eks_cluster
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eks_cluster
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "18.20.5"
@@ -83,6 +121,9 @@ module "eks" {
         }
     }
   }
+  depends_on = [
+    aws_iam_role_policy_attachment.amazon_eks_cluster_policy
+  ]
 }
 
 data "aws_eks_cluster" "cluster" {
