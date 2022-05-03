ea5902542b3911ca04037d9fd2633fd9e9255fed
reecexlm <reecexlm@gmail.com>
2022-05-02 17:23:10 -0700

    try to fix

--- helm_provider.tf
+++ helm_provider.tf
@@ -3,10 +3,6 @@ provider "helm" {
     host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
     token                  = data.aws_eks_cluster_auth.cluster-auth.token
-    #exec {
-    #  api_version = "client.authentication.k8s.io/v1alpha1"
-    #  args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
-    #  command     = "aws"
-    #}
+    load_config_file       = false
   }
 }
