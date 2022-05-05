017b797c25cf68019d0ed6a63a5f786345b26bef
reecexlm <reecexlm@gmail.com>
2022-05-05 14:35:02 -0700

    rtyhdfgh

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -357,5 +357,5 @@ data "aws_eks_cluster_auth" "cluster-auth" {
 resource "aws_iam_role_policy_attachment" "additional" {
    for_each = module.eks.eks_managed_node_groups
    policy_arn = aws_iam_policy.elb_controller_policy.arn
-   role = each.value.eks_cluster
+   role = each.value.iam_role_name
 }
