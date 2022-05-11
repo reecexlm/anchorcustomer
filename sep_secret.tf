14b2e761bc6e835e3a1d499c8303cdabe4eda38d
reecexlm <reecexlm@gmail.com>
2022-05-11 11:03:09 -0700

    secret json decode

--- sep_secret.tf
+++ sep_secret.tf
@@ -1,16 +1,16 @@
+data "aws_secretsmanager_secret" "sep_cert" {
+  arn = "arn:aws:secretsmanager:us-east-2:245943599471:secret:sep_cert-m8hg7I"
+}
 
-
-resource "kubernetes_secret" "sep_cert" {
+esource "kubernetes_secret" "sep_cert" {
   metadata {
     name = "anchor-platform-sep-server-cert"
   }
 
-  data "aws_secretsmanager_secret" "sep_cert" {
-  arn = "arn:aws:secretsmanager:us-east-2:245943599471:secret:sep_cert-m8hg7I"
-}
+
   data = {
-    "tls.crt" = data.aws_secretsmanager_secret.sep_cert.crt.secret_string
-    "tls.key" = data.aws_secretsmanager_secret.sep_cert.key.secret_string
+    "tls.crt" = jsondecode(data.aws_secretsmanager_secret.sep_cert.secret_string)["crt"]
+    "tls.key" = jsondecode(data.aws_secretsmanager_secret.sep_cert.secret_string)("key")
 }
   type = "kubernetes.io/tls"
 }
