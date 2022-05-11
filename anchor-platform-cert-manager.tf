ae8afe545f22f2a29d256526610321961b5c801b
reecexlm <reecexlm@gmail.com>
2022-05-11 15:42:20 -0700

    clusterfissuer

--- anchor-platform-cert-manager.tf
+++ anchor-platform-cert-manager.tf
@@ -12,3 +12,19 @@ resource "helm_release" "cert-manager" {
   timeout          = 600
 }
 
+resource "helm_release" "cert-issuer" {
+  name             = "cert-issuer"
+  chart            = "./charts/cert-issuer"
+  namespace        = "cert-manager"
+  create_namespace = true
+  wait             = true
+  reset_values     = true
+  max_history      = 3
+  timeout          = 600
+  depends_on = [resource.helm_release.cert-manager, resource.helm_release.reference]
+
+      #values = []#"${path.module}/anchor-platform-sep-server-values.yaml"]
+    values = [templatefile("${path.module}/anchor-platform-sep-server-values.yaml",
+    local.sep_template_vars)]
+
+}
