6c00f92ccfd772ebfaf7bebcc5cf104be5f31d79
reecexlm <reecexlm@gmail.com>
2022-05-11 10:54:49 -0700

    typo

--- sep_secret.tf
+++ sep_secret.tf
@@ -5,12 +5,12 @@ resource "kubernetes_secret" "sep_cert" {
     name = "anchor-platform-sep-server-cert"
   }
 
-  data "aws_secretsmanager_secret" "sep-cert" {
+  data "aws_secretsmanager_secret" "sep_cert" {
   arn = "arn:aws:secretsmanager:us-east-2:245943599471:secret:sep_cert-m8hg7I"
 }
   data = {
-    "tls.crt" = data.aws_secretsmanager_secret.sep-cert.crt.secret_string
-    "tls.key" = data.aws_secretsmanager_secret.sep-cert.key.secret_string
+    "tls.crt" = data.aws_secretsmanager_secret.sep_cert.crt.secret_string
+    "tls.key" = data.aws_secretsmanager_secret.sep_cert.key.secret_string
 }
   type = "kubernetes.io/tls"
 }
