88df5de0782de62b5cafc4a24188512463ac5124
reecexlm <reecexlm@gmail.com>
2022-05-11 10:43:09 -0700

    tryu2

--- sep_secret.tf
+++ sep_secret.tf
@@ -9,8 +9,8 @@ resource "kubernetes_secret" "sep_cert" {
   arn = "arn:aws:secretsmanager:us-east-2:245943599471:secret:sep_cert-m8hg7I"
 }
   data = {
-    "tls.crt" = data.aws_secretsmanager_secret.sep-cert.crt
-    "tls.key" = data.aws_secretsmanager_secret.sep-cert.key
+    "tls.crt" = data.aws_secretsmanager_secret.sep-cert.crt.secret_string
+    "tls.key" = data.aws_secretsmanager_secret.sep-cert.key.secret_string
 }
   type = "kubernetes.io/tls"
 }
