2e2d79166792fd3c871660235f6ef64361be3d8f
reecexlm <reecexlm@gmail.com>
2022-05-11 11:12:08 -0700

    well 3rd time charm

--- sep_secret.tf
+++ sep_secret.tf
@@ -2,6 +2,10 @@ data "aws_secretsmanager_secret" "sep_cert" {
   arn = "arn:aws:secretsmanager:us-east-2:245943599471:secret:sep_cert-m8hg7I"
 }
 
+data "aws_secretsmanager_secret_version" "current" {
+  secret_id = data.aws_secretsmanager_secret.sep_cert.id
+}
+
 resource "kubernetes_secret" "sep_cert" {
   metadata {
     name = "anchor-platform-sep-server-cert"
@@ -9,8 +13,8 @@ resource "kubernetes_secret" "sep_cert" {
 
 
   data = {
-    "tls.crt" = jsondecode(data.aws_secretsmanager_secret.sep_cert.secret_string)["crt"]
-    "tls.key" = jsondecode(data.aws_secretsmanager_secret.sep_cert.secret_string)["key"]
+    "tls.crt" = jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["crt"]
+    "tls.key" = jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["key"]
 }
   type = "kubernetes.io/tls"
 }
