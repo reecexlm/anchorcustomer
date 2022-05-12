a770a9c0f985d0d13c6b0df2038abd477cb77b50
reecexlm <reecexlm@gmail.com>
2022-05-12 15:59:04 -0700

    dsdfasdfdsafsdfasdf

--- kafka.tf
+++ kafka.tf
@@ -52,7 +52,7 @@ resource "aws_msk_cluster" "anchorkafka" {
   }
 
   configuration_info {
-    revision = latest
+    revision = resource.aws_msk_configuration.anchor_kafka_config.latest_revision
     arn = resource.aws_msk_configuration.anchor_kafka_config.arn
   }
 }
