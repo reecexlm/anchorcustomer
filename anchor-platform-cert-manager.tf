d2e45e624f566c9d1532f326437b2c08d6e93bda
reecexlm <reecexlm@gmail.com>
2022-05-12 05:53:48 -0700

    cupa edits

--- anchor-platform-cert-manager.tf
+++ anchor-platform-cert-manager.tf
@@ -28,6 +28,7 @@ resource "helm_release" "cert-issuer" {
   name             = "cert-issuer"
   chart            = "./charts/cert-issuer"
   namespace        = "cert-manager"
+  secretName       = "secret"
   create_namespace = true
   wait             = true
   reset_values     = true
